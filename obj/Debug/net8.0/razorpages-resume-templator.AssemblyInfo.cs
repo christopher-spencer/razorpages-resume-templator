21ec49c604446911595382c7a4265d2b55026bbc
thebellywolf <cdspenzer@gmail.com>
2024-02-05 13:09:40 -0500

    update

--- obj/Debug/net8.0/razorpages-resume-templator.AssemblyInfo.cs
+++ obj/Debug/net8.0/razorpages-resume-templator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("razorpages-resume-templator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abfe610d19c64660fff8cf49d5c10dc9bb06685f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76131c6b90177c88669594257da4fba67ad71a01")]
 [assembly: System.Reflection.AssemblyProductAttribute("razorpages-resume-templator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("razorpages-resume-templator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
