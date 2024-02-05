76131c6b90177c88669594257da4fba67ad71a01
thebellywolf <cdspenzer@gmail.com>
2024-02-05 13:05:21 -0500

    Initial commit

---
+++ Models/Project.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace ResumeTemplater.Models
+{
+	public class Project
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public int Year { get; set; }
+    public string Tools { get; set; }
+	}
+}
